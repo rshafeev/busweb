2f7c9c1dca355c8881d872710d61dd345648b2e7
romario <romero_sh@mail.ru>
2012-05-01 14:46:14 +0300

    add maven configure

--- src/com/pgis/bus/server/listeners/BasicServiceListener.java
+++ src/com/pgis/bus/server/listeners/BasicServiceListener.java
@@ -2,15 +2,17 @@ package com.pgis.bus.server.listeners;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.sql.DataSource;
 
+
+import org.apache.tomcat.jdbc.pool.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.pgis.bus.server.db.DBConnectionFactory;
+import com.pgis.bus.data.DBConnectionFactory;
 
 
 public class BasicServiceListener implements ServletContextListener {
@@ -26,10 +28,10 @@ public class BasicServiceListener implements ServletContextListener {
     {
     	DBConnectionFactory.close();
     }
-
     @Override
     public void contextInitialized(ServletContextEvent sce)
     {
+    	//DataSource source = initDataSource("jdbc/busPoolDB");
     	DBConnectionFactory.init("jdbc/busPoolDB");
     	log.debug("contextInitialized");
 
