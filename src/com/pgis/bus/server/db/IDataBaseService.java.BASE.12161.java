27cb05f19628723e76331f122f40e31710ac6f0e
marianna <marianna@marianna-MS-7529.(none)>
2012-06-22 14:57:31 +0300

    Com before  pull

--- src/com/pgis/bus/server/db/IDataBaseService.java.BASE.12161.java
+++
@@ -1,8 +0,0 @@
-package com.pgis.bus.server.db;
-import com.pgis.bus.server.db.orm.*;
-public interface IDataBaseService {
-
-	Station getStation(int id);
-	Station getStationByName(String name,Language lang);
-	
-}
