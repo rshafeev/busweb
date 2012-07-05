b1a85ca0418115c22f85f47da825b2d735474577
Roman <roman.shafeyev@gmail.com>
2012-07-05 22:22:27 +0300

    markers

--- Temp/marker.js
+++ Temp/marker.js
@@ -1,13 +1,16 @@
-	var marker_image_A;
-	var marker_image_B;
-function init()
-{
-	var marker_image_A='http://www.eway.in.ua/images/from_ru.png';
-	var marker_image_B='http://www.eway.in.ua/images/to_ru.png';
-}
-
+var marker_image_A;
+var marker_image_B;
+var curr_image;
 var geocoder;
 var map;
+
+function init()
+{
+	marker_image_A='http://www.eway.in.ua/images/from_ru.png';
+	marker_image_B='http://www.eway.in.ua/images/to_ru.png';
+	curr_image = marker_image_A;
+}
+
 function initialize() {
 	geocoder = new google.maps.Geocoder();
 	var latlng = new google.maps.LatLng(56.8848, 14.7730);
@@ -27,24 +30,30 @@ function initialize() {
 	      },
 	      drawingMode: google.maps.drawing.OverlayType.MARKER,
 	      markerOptions: {
-	      	  icon: image =  new google.maps.MarkerImage('http://www.eway.in.ua/images/from_ru.png') /*тут вид иконки, какой хочешь рисуночек, вот сюда надо как-то передать функцией нужную картинку, а как это сделать я не знаю :(*/
+	      	  icon: image =  curr_image/*тут вид иконки, какой хочешь рисуночек, вот сюда надо как-то передать функцией нужную картинку, а как это сделать я не знаю :(*/
 	      },
 	      
 	});
 	drawingManager.setMap(map);
 	google.maps.event.addListener(drawingManager, 'markercomplete', function(marker) {
-	      console.log(marker.getPosition().toString())
+	      //console.log(marker.getPosition().toString())
 	});
 
 	google.maps.event.addListener(drawingManager, 'overlaycomplete', function(event) {
 	      if (event.type == google.maps.drawing.OverlayType.MARKER) {
-	      	  console.log('Установлен маркер')
+	      	 // console.log('Marker was setup')
+		  if(curr_image == marker_image_A){
+		    drawingManager.markerOptions = {icon:image=marker_image_B};
+		    drawingManager.setMap(map);
+		   // curr_image
+		  }
 	      }
 	});
 	  google.maps.event.addListener(map, 'click', function(event) {
-	    placeMarker(event.latLng);moremaker(event.latLng);
+	    	  console.log('clicked on the map')
+	    //placeMarker(event.latLng);moremaker(event.latLng);
 	  });
-
+	init();
 	/*function codeAddress() {
 	var address = document.getElementById("address").value;
 	geocoder.geocode({
