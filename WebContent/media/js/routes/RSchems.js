88097fc0d74ee19061a4d0b0a05a5ccf77afad1b
Marianna <rm@premiumgis.com>
2012-11-25 20:17:15 +0200

    errors map in js

--- WebContent/media/js/routes/RSchems.js
+++ WebContent/media/js/routes/RSchems.js
@@ -2,20 +2,14 @@
 function RSchems() {
 	this.rightPanelVisible = false;
 	this.map = null;
-	this.googleMap = null;
-	
 	this.getMap = function(){
 		return this.map;
-	};
-	this.getGoogleMap = function() {
-		return this.googleMap;
 	};
 	this.main = function() {
 		var T = this;
 		$(window).bind("resize", function(e) {
 		T.on_resize_window('#map_container', 134, 0);
 		});
-		this.googleMap = new RSchems();
 		var latlng = new google.maps.LatLng(-34.397, 150.644);
 		var myOptions = {
 			zoom : 5,
@@ -40,7 +34,7 @@ function RSchems() {
 		var map = getRSchems().getGoogleMap();
 		if (map != null) {
 
-			google.maps.event.trigger(map.getMap(), 'resize');
+			google.maps.event.trigger(this.map, 'resize');
 		}
 		
 		/*$('#panel_scrollbar').tinyscrollbar_update();	*/
