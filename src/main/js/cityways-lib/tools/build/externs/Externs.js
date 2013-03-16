bc3670b709bbc697b64ab5cbc1e462115f6d83c3
Roman <roman.shafeyev@gmail.com>
2013-03-16 16:46:20 +0200

    add build folder

---
+++ src/main/js/cityways-lib/tools/build/externs/Externs.js
@@ -0,0 +1,35 @@
+
+// Check the console when using Firebug Lite
+    var console;
+
+// Proj4js
+    var Proj4js = {Proj: function(){}};
+
+// Google Maps
+    var GMap2;
+    var G_NORMAL_MAP;
+    var GEvent;
+    var GLatLngBounds = function(){};
+    var GSize = function(x, y){};
+    var GPoint = function(x, y){};
+    var GLatLng = function(lat, lon){};
+    
+// Multimap
+    var MultimapViewer = function(div){};
+    var MMLatLon = function(lat, lon){};
+    var MMPoint = function(x, y){};
+
+//VirtualEarth
+    var VEMap = function(name){};
+    var VEPixel = function(x, y){};
+    var VELatLong = function(lat, lon){};
+    var Msn = {VE:{}};
+
+// Yahoo
+    var YMap = function(div, type, size){};
+    var YGeoPoint = function(lat, lon){};
+    var YCoordPoint = function(x, y){};
+    var YSize = function(w, h){};
+
+// Jquery extensions
+    var selectbox_initialize = function(){};
